ef9f27ba6e9b056815897f995ba9db5ef68f579f
vBlackOut <26262208+vBlackOut@users.noreply.github.com>
2025-05-08 22:38:31 +0200

    0.5.14

--- src/stats.rs
+++ src/stats.rs
@@ -3,16 +3,10 @@ use crate::AppState;
 
 
 pub async fn stats(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
-    let peer_addr = req.peer_addr();
-    if let Some(addr) = peer_addr {
-        if !addr.ip().is_loopback() {
-            return HttpResponse::Forbidden().body("Access denied");
-        }
-    }
 
     let expected_token = &data.config.token_admin;
     let auth_header = req.headers().get("X-Auth-Token");
-    println!("{}", expected_token);
+
     match auth_header {
         Some(token) if *token == *expected_token => {
             let stats = data.counter.lock().unwrap().get_all_tokens_json();
