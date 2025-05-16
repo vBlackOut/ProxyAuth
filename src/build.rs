c772296686c252453482d59322a39e8116167942
vBlackOut <26262208+vBlackOut@users.noreply.github.com>
2025-05-16 06:57:58 +0200

    Rollback build anthropiue

--- src/build.rs
+++ src/build.rs
@@ -1,5 +1,5 @@
 use rand::seq::SliceRandom;
-use rand::{Rng, SeedableRng, rngs::OsRng};
+use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
 use std::fs;
 use std::io::Write;
@@ -7,17 +7,15 @@ use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
-    let mut rng = OsRng;
-
+    let mut rng = rand::thread_rng();
     let build_rand = rng.gen_range(1..999_999_999);
-    let build_seed = rng.gen_range(1..999_999_999);
-    let build_seed2 = rng.gen_range(1..999_999_999);
+    let build_seed = rng.gen_range(1..999);
 
     let build_time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs()
-        .to_string();
+    .duration_since(UNIX_EPOCH)
+    .unwrap()
+    .as_secs()
+    .to_string();
 
     println!("cargo:rustc-env=BUILD_TIME={}", build_time);
     println!("cargo:rustc-env=BUILD_RAND={}", build_rand);
@@ -33,17 +31,17 @@ fn main() {
         "token_id".to_string(),
     ];
 
-    let mut shuffle_rng = ChaCha8Rng::seed_from_u64((((build_seed*build_seed2)/2)*3));
+    let mut shuffle_rng = ChaCha8Rng::seed_from_u64(build_seed);
     fields.shuffle(&mut shuffle_rng);
 
     let const_string = format!(
         "pub const SHUFFLED_ORDER: [&str; {}] = [{}];",
         fields.len(),
-        fields
-            .iter()
-            .map(|s| format!("\"{}\"", s))
-            .collect::<Vec<_>>()
-            .join(", ")
+                               fields
+                               .iter()
+                               .map(|s| format!("\"{}\"", s))
+                               .collect::<Vec<_>>()
+                               .join(", ")
     );
 
     let out_dir = std::env::var("OUT_DIR").unwrap();
