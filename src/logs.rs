7cf1c173e99df38cff1957778be334db3288e55d
vBlackOut <26262208+vBlackOut@users.noreply.github.com>
2025-06-11 13:05:27 +0200

    Prepare version 0.7.8

---
+++ src/logs.rs
@@ -0,0 +1,67 @@
+use actix_web::{HttpResponse, HttpRequest, Responder, web};
+use once_cell::sync::Lazy;
+use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tracing_subscriber::fmt::MakeWriter;
+use crate::AppState;
+
+
+pub static LOG_BUFFER: Lazy<Arc<Mutex<Vec<String>>>> = Lazy::new(|| Arc::new(Mutex::new(Vec::new())));
+
+#[derive(Clone)]
+pub struct ChannelWriter {
+    sender: Arc<UnboundedSender<String>>,
+}
+
+impl Write for ChannelWriter {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let s = String::from_utf8_lossy(buf).to_string();
+        let _ = self.sender.send(s);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+pub struct ChannelLogWriter {
+    pub sender: Arc<UnboundedSender<String>>,
+}
+
+impl<'a> MakeWriter<'a> for ChannelLogWriter {
+    type Writer = ChannelWriter;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        ChannelWriter {
+            sender: self.sender.clone(),
+        }
+    }
+}
+
+pub async fn log_collector(mut rx: UnboundedReceiver<String>) {
+    while let Some(log) = rx.recv().await {
+        let mut logs = LOG_BUFFER.lock().unwrap();
+        logs.push(log);
+        if logs.len() > 1000 {
+            logs.remove(0);
+        }
+    }
+}
+
+pub async fn get_logs(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    let expected_token = &data.config.token_admin;
+    let auth_header = req.headers().get("X-Auth-Token");
+
+    match auth_header {
+        Some(token) if token == expected_token => {
+            let logs = LOG_BUFFER.lock().unwrap();
+            HttpResponse::Ok()
+            .content_type("text/plain")
+            .body(logs.join(""))
+        }
+        _ => HttpResponse::Unauthorized().body("Invalid or missing token"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod stats;
 mod timezone;
 mod tls;
 mod build_info;
+mod logs;
 
 use actix_governor::{Governor, GovernorConfigBuilder};
 use actix_web::{App, HttpServer, web};
@@ -26,6 +27,8 @@ use stats::stats::stats as metric_stats;
 use std::{fs, sync::Arc, io, process, time::Duration};
 pub use stats::tokencount::CounterToken;
 use tracing_loki::url::Url;
+use logs::{log_collector, ChannelLogWriter, get_logs};
+use tokio::sync::mpsc::{UnboundedSender, UnboundedReceiver, unbounded_channel};
 use tracing_subscriber::{
     Layer, filter, EnvFilter, filter::LevelFilter,
     layer::SubscriberExt, util::SubscriberInitExt,
@@ -153,8 +156,21 @@ async fn main() -> std::io::Result<()> {
             tracing_subscriber::fmt()
             .with_env_filter(EnvFilter::new("proxyauth=trace"))
             .with_timer(LocalTime)
-            .with_ansi(true)
+            .with_max_level(tracing::Level::INFO)
             .init();
+        }
+
+        if logs == "http" {
+            let (tx, rx) = unbounded_channel::<String>();
+
+            tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::new("proxyauth=trace"))
+            .with_timer(LocalTime)
+            .with_writer(ChannelLogWriter { sender: tx.clone().into() })
+            .with_max_level(tracing::Level::INFO)
+            .init();
+
+            tokio::spawn(log_collector(rx));
             // tracing_subscriber::fmt()
             // .with_writer(|| std::io::sink())
             // .init();
@@ -253,6 +269,7 @@ async fn main() -> std::io::Result<()> {
                     .app_data(state.clone())
                     .service(web::resource("/auth").route(web::post().to(auth)))
                     .service(web::resource("/adm/stats").route(web::get().to(metric_stats)))
+                    .service(web::resource("/adm/logs").route(web::get().to(get_logs)))
                     .default_service(web::to(global_proxy).wrap(Governor::new(&governor_proxy_conf)))
             })
             .workers((config.worker as u8).into())
@@ -284,6 +301,7 @@ async fn main() -> std::io::Result<()> {
                         ),
                     )
                     .service(web::resource("/adm/stats").route(web::get().to(metric_stats)))
+                    .service(web::resource("/adm/logs").route(web::get().to(get_logs)))
                     .default_service(web::to(global_proxy))
             })
             .workers((config.worker as u8).into())
@@ -328,6 +346,7 @@ async fn main() -> std::io::Result<()> {
                         ),
                     )
                     .service(web::resource("/adm/stats").route(web::get().to(metric_stats)))
+                    .service(web::resource("/adm/logs").route(web::get().to(get_logs)))
                     .default_service(web::to(global_proxy).wrap(Governor::new(&governor_proxy_conf)))
             })
             .workers((config.worker as u8).into())
@@ -344,6 +363,7 @@ async fn main() -> std::io::Result<()> {
                     .app_data(state.clone())
                     .service(web::resource("/auth").route(web::post().to(auth)))
                     .service(web::resource("/adm/stats").route(web::get().to(metric_stats)))
+                    .service(web::resource("/adm/logs").route(web::get().to(get_logs)))
                     .default_service(web::to(global_proxy))
             })
             .workers((config.worker as u8).into())
@@ -363,6 +383,7 @@ async fn main() -> std::io::Result<()> {
                     .app_data(state.clone())
                     .service(web::resource("/auth").route(web::post().to(auth)))
                     .service(web::resource("/adm/stats").route(web::get().to(metric_stats)))
+                    .service(web::resource("/adm/logs").route(web::get().to(get_logs)))
                     .default_service(web::to(global_proxy))
             })
             .workers((config.worker as u8).into())
