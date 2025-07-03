bcc020ad71793a6cca5b6cf86bfe2f3b8c195990
vBlackOut <26262208+vBlackOut@users.noreply.github.com>
2025-07-03 14:05:19 +0200

    Add Version command

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -3,6 +3,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(name = "proxyauth")]
 #[command(about = "Manage proxyauth system", long_about = None)]
+#[command(version)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Option<Commands>,
